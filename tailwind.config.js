52fe1333f2538b8d6c7913728094421866763750
Jayen Hasan <jayenhasan2003@gmail.com>
2023-09-02 18:36:47 +0600

    course page testimonial section

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#ED6326',
+      'secondary': '#FBA91E',
+      'green': '#ABD03B',
+      'sky': '#41A0D8',
+      'purple': '#8253A0',
+      'red': '#F16768',
+      'dark-blue': '#1B1A48',
+      'white': '#FFFFFF',
+      'secondary-text': '#5F5F7F'
+    },
+    extend: {
+      fontFamily : {
+        'primary' : 'Exo2'
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      }
+    },
+  },
+  plugins: [],
+}
+
